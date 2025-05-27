4ed3197eaf41aa7d95f732934bb1b6944414121b
Jose Llave <rodolfollave123@gmail.com>
2025-05-27 03:53:16 -0500

    Agregando llaves al valor de la prop en la variable texto

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <>
       <Navbar logoUrl={"https://i.ibb.co/r2pQ5CN5/logonuevo.png"} />
-      <ItemListContainer texto="Lista de Entradas para Conciertos" />
+      <ItemListContainer texto={"Lista de Entradas para Conciertos"} />
     </>
   )
 }
