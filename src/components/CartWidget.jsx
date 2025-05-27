85a74eef0125e96944aee1d120000e46e03c6eb9
Jose Llave <rodolfollave123@gmail.com>
2025-05-27 03:46:50 -0500

    Creando el componente de CartWidget para el icono de carrito junto con su contador estatico

---
+++ src/components/CartWidget.jsx
@@ -0,0 +1,10 @@
+function CartWidget() {
+    return (
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+            <span style={{ fontSize: '28px' }}>🛒</span>
+            <span style={{ color: 'white', marginLeft: '5px' }}>2</span>
+        </div>
+    )
+}
+
+export default CartWidget
