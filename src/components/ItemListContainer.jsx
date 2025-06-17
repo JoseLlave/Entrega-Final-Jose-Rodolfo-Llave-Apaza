6680b34bd2fd024600ce3a5c01f7473768495950
Jose Llave <rodolfollave123@gmail.com>
2025-06-17 02:28:01 -0500

    Se actualizo el componente ItemListContainer usando promesas y la api dummyjson

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,8 +1,30 @@
-function ItemListContainer({ texto }) {
-    return (
-        <div>
-            <h1>{texto}</h1>
-        </div>
-    );
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router';
+import ItemList from './ItemList';
+
+function ItemListContainer() {
+  const [items, setItems] = useState([]);
+  const { categoryName } = useParams();
+
+  useEffect(() => {
+    const getProducts = new Promise((resolve) => {
+      let url;
+      
+      if (categoryName) {
+        url = `https://dummyjson.com/products/category/${categoryName}`;
+      } else {
+        url = 'https://dummyjson.com/products';
+      }
+      
+      fetch(url)
+        .then(res => res.json())
+        .then(data => resolve(data.products || []));
+    });
+
+    getProducts.then(products => setItems(products));
+  }, [categoryName]);
+
+  return <ItemList items={items} />;
 }
+
 export default ItemListContainer;
