6a4016c69bbcd54ba6c5d24da0a92af930cc92bc
Jose Llave <rodolfollave123@gmail.com>
2025-06-17 02:30:45 -0500

    Se actualizo el archivo app para las rutas dinamicas

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
-import './App.css'
-import Navbar from './components/Navbar'
-import ItemListContainer from './components/ItemListContainer'
+import { BrowserRouter, Routes, Route } from "react-router";
+import NavBar from "./components/Navbar/NavBar";
+import ItemListContainer from "./components/ItemListContainer";
+import ItemDetailContainer from "./components/ItemDetail/ItemDetailContainer";
 
 function App() {
   return (
-    <>
-      <Navbar logoUrl={"https://i.ibb.co/r2pQ5CN5/logonuevo.png"} />
-      <ItemListContainer texto={"Lista de Entradas para Conciertos"} />
-    </>
-  )
+    <BrowserRouter>
+      <NavBar />
+      <main className="container-fluid mt-5 pt-4 px-4">
+        <div className="row justify-content-center">
+          <div className="col-12 col-xxl-10">
+            <Routes>
+              <Route path="/" element={<ItemListContainer />} />
+              <Route path="/category/:categoryName" element={<ItemListContainer />} />
+              <Route path="/item/:id" element={<ItemDetailContainer />} />
+            </Routes>
+          </div>
+        </div>
+      </main>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
