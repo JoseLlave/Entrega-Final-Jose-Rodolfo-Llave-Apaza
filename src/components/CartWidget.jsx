3e9a3ceb110320a70b5a06e0785b851780a0d259
Jose Llave <rodolfollave123@gmail.com>
2025-06-17 02:13:50 -0500

    Actualizacion del componente CartWidget con bootstrap

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -1,10 +1,15 @@
+import { Link } from 'react-router';
+import Badge from 'react-bootstrap/Badge';
+
 function CartWidget() {
-    return (
-        <div style={{ display: 'flex', alignItems: 'center' }}>
-            <span style={{ fontSize: '28px' }}>🛒</span>
-            <span style={{ color: 'white', marginLeft: '5px' }}>2</span>
-        </div>
-    )
+  return (
+    <Link to="/cart" className="position-relative text-decoration-none">
+      <i className="bi bi-cart3 fs-3 text-white"></i>
+      <Badge pill bg="danger" className="position-absolute top-0 start-100 translate-middle">
+        2
+      </Badge>
+    </Link>
+  );
 }
 
-export default CartWidget
+export default CartWidget;
