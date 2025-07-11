4a1c9c96745085b2fba7bc54e0c6e1e596bc4bd6
Jose Llave <rodolfollave123@gmail.com>
2025-07-11 18:27:18 -0500

    Corrigiendo ruta

--- src/components/Navbar/NavBar.jsx
+++ src/components/Navbar/NavBar.jsx
@@ -5,7 +5,7 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import CartWidget from '../CartWidget';
-import styles from './NavBar.module.css';
+import styles from './Navbar.module.css';
 
 function NavBar() {
   return (
